fc7fecaf307d753bd11eebbc96eaccb5267349b2
T4skFoRcE <t4skforce@tk45nb.neumair.net>
2017-12-24 11:26:51 +0100

    inital

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# epdif
-EPD hardware interface implements (GPIO, SPI)
+# About
+This repo was created to provide a convenient way to install GDEM0213E26LT 2.13" e-paper library for Raspberry Pi.
+The original code is provided by [Waveshare GitHub](https://github.com/soonuse/epd-library-python).
---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+setup(name='epdif',
+      version='0.1',
+      description='EPD hardware interface implements (GPIO, SPI)',
+      url='https://github.com/t4skforce/epdif',
+      author='Yehui from Waveshare',
+      author_email='service@waveshare.com',
+      license='GPL',
+      packages=['epdif'],
+      install_requires=[
+          'spidev',
+          'RPi.GPIO'
+      ],
+      classifiers = [
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3"
+      ],
+      zip_safe=False)
