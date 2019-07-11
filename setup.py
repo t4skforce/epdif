a6162804530f9bd787a01ff6a8cc3aa88a6d6999
t4skforce <t4skforce1337@gmail.com>
2019-07-11 20:30:14 +0000

    Bump version: 0.1.0 → 0.1.1

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='epdif',
-      version='0.1.0',
+      version='0.1.1',
       description='EPD hardware interface implements (GPIO, SPI)',
       url='https://github.com/t4skforce/epdif',
       author='Yehui from Waveshare',
